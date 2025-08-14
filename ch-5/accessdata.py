d8e145383701a1d88120b32777206b7f036ce239
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2025-08-14 20:35:42 +0530

    add ch-5

---
+++ ch-5/accessdata.py
@@ -0,0 +1,19 @@
+# Access data
+
+d1 = {
+    "Pushtak" : "book",
+    "Pankha" : "Fan",
+    "Vishay" : "Subject",
+    "darvaja" : "Door"
+}
+
+print(d1)
+
+#Access key & values
+
+print(d1.keys())
+print(d1.values())
+
+# specific 
+print("First:", d1["Pushtak"])
+
